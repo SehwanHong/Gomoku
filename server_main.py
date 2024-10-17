2b9e643f1278217acf1de4204178c979017ee520
Sehwan Hong <samhong657@gmail.com>
2024-10-17 11:15:54 +0900

    fix typo in server_main.py

--- server_main.py
+++ server_main.py
@@ -83,8 +83,8 @@ def parse_args():
     parser.add_argument('--time_search', action='store_true', help="change search from iteration to time")
 
     parser.add_argument('--iter', default=10, help='iteration number')
-    parser.add_arguement('--self_play', action="store_true" , help='option for selfplay')
-    parser.add_arguement('--evaluate_model', action="store_true" , help='option for evaluating two model')
+    parser.add_argument('--self_play', action="store_true" , help='option for selfplay')
+    parser.add_argument('--evaluate_model', action="store_true" , help='option for evaluating two model')
     return parser.parse_args()
 
 if __name__ == "__main__":
