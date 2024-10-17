534ae2701c3d12e6b31963858d564ea9e881084f
Sehwan Hong <samhong657@gmail.com>
2024-10-17 09:56:53 +0900

    add script for testing training

---
+++ script/01_train_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+##SBATCH --nodelist=nv180
+
+#SBATCH --time=10:00:00
+#SBATCH -p 40g
+#SBATCH --nodes=1            # This needs to match Trainer(num_nodes=...)
+#SBATCH --ntasks-per-node=1  # This needs to match Trainer(devices=...)
+#SBATCH --mem=16gb
+#SBATCH --cpus-per-task=1
+#SBATCH -o ./logs/%A.txt
+
+export RUN_NAME="run_$SLURM_JOB_ID"
+
+# run script from above
+srun --container-image /purestorage/project/shhong/enroot_images/torch2.sqsh \
+    --container-mounts /purestorage:/purestorage,/purestorage/project/shhong/tmp:/home/$USER/.cache \
+    --no-container-mount-home \
+    --container-writable \
+    --container-workdir /purestorage/project/shhong/Gomoku/ \
+    bash -c "
+    pip install lightning;
+    python train.py --run_name $RUN_NAME --data_dir ./data/ --model_dir ./model/;
+    " 
+    
